9136fabab408b0a1114d5cf3c9046450b8fb98c9
CarolPinerosTrujillo <caroll25m@gmail.com>
2026-08-13 22:14:37 -0500

    Inicio repo

---
+++ src/Ave.java
@@ -0,0 +1,8 @@
+public class Ave extends Animal{
+    private double pesoGramos;
+    public Ave(String nombre, int edad, String nombreDueno, double pesoGramos) {
+        super(nombre, edad, nombreDueno);
+        this.pesoGramos = pesoGramos;
+    }
+
+}
